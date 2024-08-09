27cd9caccf20b85d092c98e0b9c4211d103f7964
xuxu156 <142868227+xuxu156@users.noreply.github.com>
2024-08-09 20:00:22 +0800

    Delete src directory

--- src/main/java/net/mcreator/stfabric/procedures/StPLUszeiProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.stfabric.procedures;
-
-import net.minecraft.world.item.ItemStack;
-
-public class StPLUszeiProcedure {
-	public static void execute(ItemStack itemstack) {
-		itemstack.shrink(1);
-	}
-}
--- src/main/java/net/mcreator/stfabric/procedures/StYouJiKongQiShiShiTiDeWeiZhiProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.stfabric.procedures;
-
-import net.minecraft.world.item.ItemStack;
-
-public class StYouJiKongQiShiShiTiDeWeiZhiProcedure {
-	public static void execute(ItemStack itemstack) {
-		itemstack.shrink(1);
-	}
-}
